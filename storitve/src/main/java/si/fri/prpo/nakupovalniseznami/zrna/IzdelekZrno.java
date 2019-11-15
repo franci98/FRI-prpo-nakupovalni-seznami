e21714115d656144c7daf8e5bc3172a23cf2c391
Tim Vucina <tim.vucina@gmail.com>
2019-11-15 13:45:55 +0100

    Dodane CRUD metode v IzdelekZrno, popravek persistance.xml

--- storitve/src/main/java/si/fri/prpo/nakupovalniseznami/zrna/IzdelekZrno.java
+++ storitve/src/main/java/si/fri/prpo/nakupovalniseznami/zrna/IzdelekZrno.java
@@ -2,14 +2,29 @@ package si.fri.prpo.nakupovalniseznami.zrna;
 
 import si.fri.prpo.nakupovalniseznami.entitete.Izdelek;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.logging.Logger;
 
 @ApplicationScoped
 public class IzdelekZrno {
+
+    private String idZrna;
+
+    @PostConstruct
+    private void init() {
+        log.info("Inicializacija zrna" + IzdelekZrno.class.getName());
+    }
+
+    @PreDestroy
+    private void destroy() {
+        log.info("Deinicializacija zrna" + IzdelekZrno.class.getName());
+    }
 
     private Logger log = Logger.getLogger(IzdelekZrno.class.getName());
 
@@ -18,9 +33,48 @@ public class IzdelekZrno {
 
     public List<Izdelek> getAllItems() {
 
-        List<Izdelek> izdelki = em.createNamedQuery("Izdelek.getAll").getResultList();
+        List<Izdelek> izdelki = em.createNamedQuery("Izdelek.getAll", Izdelek.class).getResultList();
 
         return izdelki;
+    }
+
+    @Transactional
+    public Izdelek create(Izdelek izdelek) {
+
+        if (izdelek != null) {
+            em.persist(izdelek);
+        }
+
+        return izdelek;
+    }
+
+    @Transactional
+    public Izdelek read(int id) {
+
+        Izdelek i = em.find(Izdelek.class, id);
+
+        return i;
+    }
+
+    @Transactional
+    public void update(int id, Izdelek izdelek) {
+
+        Izdelek i = em.find(Izdelek.class, id);
+
+        izdelek.setId(i.getId());
+        em.merge(izdelek);
+    }
+
+    @Transactional
+    public int delete(int id) {
+
+        Izdelek i = em.find(Izdelek.class, id);
+
+        if (i != null) {
+            em.remove(i);
+        }
+
+        return id;
     }
 
 }
