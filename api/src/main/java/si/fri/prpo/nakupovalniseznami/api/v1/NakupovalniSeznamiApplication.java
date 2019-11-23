8237b3b03ab306c5494870b8a6c57bb662e9b8a1
Franci Klavž <franciklavz@gmail.com>
2019-11-23 18:39:04 +0100

    Add application

---
+++ api/src/main/java/si/fri/prpo/nakupovalniseznami/api/v1/NakupovalniSeznamiApplication.java
@@ -0,0 +1,8 @@
+package si.fri.prpo.nakupovalniseznami.api.v1;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("v1")
+public class NakupovalniSeznamiApplication extends Application {
+}
