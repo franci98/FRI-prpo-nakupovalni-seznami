cbb91819feb8d0f84d6c00bd670d29f901a03c22
Franci Klavž <franciklavz@gmail.com>
2019-12-03 10:22:08 +0100

    Dodana dokumentacija OpenAPI za UporabnikSource

--- api/src/main/java/si/fri/prpo/nakupovalniseznami/api/v1/NakupovalniSeznamiApplication.java
+++ api/src/main/java/si/fri/prpo/nakupovalniseznami/api/v1/NakupovalniSeznamiApplication.java
@@ -1,8 +1,36 @@
 package si.fri.prpo.nakupovalniseznami.api.v1;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
 
+@SecurityScheme(name = "openid-connect", type = SecuritySchemeType.OPENIDCONNECT,
+        openIdConnectUrl = "http://auth-server-url/.well-known/openid-configuration")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API za nakupovalni seznam",
+                version = "v1",
+                description = "API, ki omogoča enostavno upravljanje z nakupovalnimi seznami.",
+                contact = @Contact(
+                        name = "Franc Klavž, Tim Vučina",
+                        url = "https://github.com/franci98",
+                        email = "franciklavz@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
+                )
+        ),
+        security = @SecurityRequirement(name = "openid-connect"),
+        servers = @Server(url ="http://localhost:8080/v1"))
 @ApplicationPath("v1")
 public class NakupovalniSeznamiApplication extends Application {
 }
--- api/src/main/java/si/fri/prpo/nakupovalniseznami/api/v1/sources/UporabnikSource.java
+++ api/src/main/java/si/fri/prpo/nakupovalniseznami/api/v1/sources/UporabnikSource.java
@@ -1,6 +1,13 @@
 package si.fri.prpo.nakupovalniseznami.api.v1.sources;
 
 import com.kumuluz.ee.rest.beans.QueryParameters;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.eclipse.jetty.server.Authentication;
 import si.fri.prpo.nakupovalniseznami.entitete.Uporabnik;
 import si.fri.prpo.nakupovalniseznami.zrna.UporabnikZrno;
 
@@ -26,6 +33,10 @@ public class UporabnikSource {
     private UporabnikZrno uporabnikZrno;
 
     @GET
+    @Operation(summary = "Pridobi vse uporabnike",
+            description = "Vrne seznam vseh uporabnikov, ki so v bazi.",
+            tags = "uporabniki"
+    )
     public Response getUsers() {
         QueryParameters query = QueryParameters.query(uriInfo.getRequestUri().getQuery()).build();
         List<Uporabnik> uporabniki = uporabnikZrno.getAllUsers(query);
@@ -40,7 +51,23 @@ public class UporabnikSource {
      */
     @GET
     @Path("{id}")
-    public Response getUser(@PathParam("id") Integer id) {
+    @Operation(
+            summary = "Pridobi uporabnika z določenim ID-jem",
+            tags = "uporabniki",
+            responses = {
+                    @ApiResponse(description = "Uporabnik",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Uporabnik.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "404", description = "Uporabnika s podanim identifikatorjem nismo našli.")
+            },
+            description = "V bazi najde in vrne uporabnika s točno določenim ID-jem-"
+    )
+    public Response getUser(
+            @Parameter(description = "Identifikator uporabnika", required = true) @PathParam("id") Integer id
+    ) {
         Uporabnik uporabnik = uporabnikZrno.get(id);
 
         if (uporabnik == null) {
@@ -50,7 +77,26 @@ public class UporabnikSource {
     }
 
     @POST
-    public Response addUser(Uporabnik uporabnik) {
+    @Operation(
+            summary = "Ustvari novega uporabnika",
+            tags = "uporabniki",
+            responses = {
+                    @ApiResponse(
+                            description = "Novi uporabnik",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Uporabnik.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Podatki niso pravilno izpolnjeni.")
+            },
+            description = "Ustvari novega uporabnika s podanimi podatki"
+    )
+    public Response addUser(
+            @RequestBody(
+                    description = "Objekt uporabnika z vsemi pripadajočimi podatki", required = true,
+                    content = @Content(schema = @Schema(implementation = Uporabnik.class))
+            ) Uporabnik uporabnik) {
         return Response
                 .status(Response.Status.CREATED)
                 .entity(uporabnikZrno.create(uporabnik))
@@ -59,7 +105,26 @@ public class UporabnikSource {
 
     @PUT
     @Path("{id}")
-    public Response updateUser(@PathParam("id") Integer userId, Uporabnik uporabnik) {
+    @Operation(
+            summary = "Posodobi uporabnika",
+            tags = "uporabniki",
+            responses = {
+                    @ApiResponse(
+                            description = "Posodobljeni uporabnik",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = Uporabnik.class)
+                            )
+                    )
+            },
+            description = "Posodobi uporabnika s podanim identifikatorjem."
+    )
+    public Response updateUser(
+            @Parameter(description = "Identifikator uporabnika", required = true) @PathParam("id") Integer userId,
+            @RequestBody(
+                    description = "Posodobljeni objekt uporabnika z vsemi pripadajočimi podatki", required = true,
+                    content = @Content(schema = @Schema(implementation = Uporabnik.class))
+            ) Uporabnik uporabnik) {
         return Response
                 .status(Response.Status.CREATED)
                 .entity(uporabnikZrno.update(userId, uporabnik))
@@ -68,7 +133,20 @@ public class UporabnikSource {
 
     @DELETE
     @Path("{id}")
-    public Response deleteUser(@PathParam("id") Integer userId) {
+    @Operation(
+            summary = "Izbriši uporabnika",
+            tags = "uporabniki",
+            responses = {
+                    @ApiResponse(
+                            description = "Identifikator izbrisanega uporabnika",
+                            content = @Content(mediaType = "text/plain")
+                    )
+            },
+            description = "Izbriše uporabnika s podanim identifikatorjem"
+    )
+    public Response deleteUser(
+            @Parameter(description = "Identifikator uporabnika", required = true) @PathParam("id") Integer userId
+    ) {
         return Response
                 .status(Response.Status.OK)
                 .entity(uporabnikZrno.delete(userId))
